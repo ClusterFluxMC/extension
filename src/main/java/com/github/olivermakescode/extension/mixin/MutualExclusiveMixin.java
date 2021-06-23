d1bb89fee60537e7b49fd24ff329d71d402e1d0a
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2021-06-23 15:55:18 -0500

    Fix doubling of enchantments w/ enchantment table

--- src/main/java/com/github/olivermakescode/extension/mixin/MutualExclusiveMixin.java
+++ src/main/java/com/github/olivermakescode/extension/mixin/MutualExclusiveMixin.java
@@ -10,8 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Enchantment.class)
 public class MutualExclusiveMixin {
     @Inject(method = "canCombine", at = @At("RETURN"), cancellable = true)
-    public void mutual(CallbackInfoReturnable<Boolean> info) {
+    public void mutual(Enchantment other, CallbackInfoReturnable<Boolean> info) {
+        Enchantment self = (Enchantment) (Object) this;
         if (extension.disableMutual.getValue())
-            info.setReturnValue(true);
+            info.setReturnValue(self != other);
     }
 }
