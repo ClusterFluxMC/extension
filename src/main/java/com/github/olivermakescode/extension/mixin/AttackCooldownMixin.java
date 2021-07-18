bfcd6edf64b3efae8e086232cad2423ef7ddf0c5
Oliver-makes-code <kookooloopdaking@gmail.com>
2021-07-18 12:13:59 -0500

    Remove qualifier

--- src/main/java/com/github/olivermakescode/extension/mixin/AttackCooldownMixin.java
+++ src/main/java/com/github/olivermakescode/extension/mixin/AttackCooldownMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public class AttackCooldownMixin {
 
-    @Inject(method= "Lnet/minecraft/server/network/ServerPlayerEntity;tick()V",at=@At("HEAD"),cancellable = true)
+    @Inject(method= "tick()V",at=@At("HEAD"),cancellable = true)
     private void attackCooldownGamerule(CallbackInfo ci) {
         ServerPlayerEntity self = (ServerPlayerEntity) (Object) this;
 
