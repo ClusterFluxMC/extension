9076a03149e74ade23f339ebc802caf49ec82af3
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-03-19 09:35:46 -0500

    update to 1.18.2

--- src/main/java/com/github/olivermakescode/extension/mixin/WaterPlaceMixin.java
+++ src/main/java/com/github/olivermakescode/extension/mixin/WaterPlaceMixin.java
@@ -4,6 +4,7 @@ import com.github.olivermakescode.extension.extension;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.BucketItem;
 import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,11 +14,11 @@ public class WaterPlaceMixin {
     @Redirect(
             at=@At(
                     value="INVOKE",
-                    target="Lnet/minecraft/fluid/Fluid;isIn(Lnet/minecraft/tag/Tag;)Z"
+                    target="Lnet/minecraft/fluid/Fluid;isIn(Lnet/minecraft/tag/TagKey;)Z"
             ),
             method="placeFluid(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/hit/BlockHitResult;)Z"
     )
-    public boolean isInRedirect(Fluid fluid, Tag<Fluid> tag) {
+    public boolean isInRedirect(Fluid fluid, TagKey<Fluid> tag) {
         if (extension.netherWater.getValue()) return false;
         return fluid.isIn(tag);
     }
